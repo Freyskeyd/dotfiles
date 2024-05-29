225afa3515399132421a37a454193e6826b77d66
Simon Paitrault <spaitrault@polygon.technology>
2024-05-29 13:22:09 +0200

    chore: adding toml format
    Signed-off-by: Simon Paitrault <spaitrault@polygon.technology>

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -33,7 +33,7 @@ return {
             require('mason-lspconfig').setup({
                 -- Replace the language servers listed here
                 -- with the ones you want to install
-                ensure_installed = { 'lua_ls', 'rust_analyzer' },
+                ensure_installed = { 'lua_ls', 'rust_analyzer', 'taplo' },
                 handlers = {
                     function(server_name)
                         require('lspconfig')[server_name].setup({})
--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -8,7 +8,7 @@ return {
             local configs = require("nvim-treesitter.configs")
 
             configs.setup({
-                ensure_installed = { 'lua' },
+                ensure_installed = { 'lua', 'toml' },
                 sync_install = false,
                 auto_install = true,
                 highlight = {
