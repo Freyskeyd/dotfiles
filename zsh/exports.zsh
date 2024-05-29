8267edbb510612f1c9c754f5da120a38e4353d07
Simon Paitrault <spaitrault@polygon.technology>
2024-05-29 09:20:55 +0200

    fixing cargo bin PATH
    Signed-off-by: Simon Paitrault <spaitrault@polygon.technology>

--- zsh/exports.zsh
+++ zsh/exports.zsh
@@ -2,7 +2,7 @@ export FZF_DEFAULT_OPTS='--height 40% --reverse --border'
 export GPG_TTY=$(tty)
 
 # export cargo bin path
-# export PATH="$HOME/.cargo/bin:$PATH"
+export PATH="$HOME/.cargo/bin:$PATH"
 export PATH="/opt/homebrew/opt/node@20/bin:$PATH"
 
 export BAT_THEME="base16-256"
